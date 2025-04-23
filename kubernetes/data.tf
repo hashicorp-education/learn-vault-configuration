95be0467ac31808475efa35b60f7e7895b771912
Jonathan Frappier <92055993+jonathanfrappier@users.noreply.github.com>
2025-04-23 15:34:54 -0400

    Update data.tf
    fix reference to secret

--- kubernetes/data.tf
+++ kubernetes/data.tf
@@ -1,8 +1,8 @@
 ## get secret data
-data "kubernetes_secret_v1" "vault-auth-service-account-secret" {
+data "kubernetes_secret_v1" "vault-auth-service-account-token" {
   depends_on = [kubernetes_secret_v1.vault-auth-service-account-token]
   metadata {
-    name      = "vault-auth-service-account-token-ckvbh"
+    name      = "vault-auth-service-account-token"
     namespace = "vault"
   }
 }
