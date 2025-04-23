a285d1eb55ff1c8e77cf29941a437daccaedc130
Jonathan Frappier <92055993+jonathanfrappier@users.noreply.github.com>
2025-04-23 15:54:03 -0400

    Create variables.tf

---
+++ vault/auth/kubernetes/variables.tf
@@ -0,0 +1,14 @@
+variable "K8S_CA_CRT" {
+  description = "Vault service address or internal name"
+  type        = string
+}
+
+variable "K8S_URL" {
+  description = "Vault service address or internal name"
+  type        = string
+}
+
+variable "K8S_VAULTSA_TOKEN" {
+  description = "Vault service address or internal name"
+  type        = string
+}
